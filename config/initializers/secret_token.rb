422815ab9d1f695f053649044882d2d708990e2e
Amine Bellakrid <a.bellakrid@gmail.com>
2011-09-06 21:23:19 +0200

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crowdguideme::Application.config.secret_token = '46159f5a841c2ea974c531850b5493808119806344ef67e5d216d334b88034d2a1eb42086bf32805fb506a270a3e629add737d2789eb4e0ce62a1570acfb79d9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Crowdguideme::Application.config.session_store :cookie_store, :key => '_crowdguideme_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Crowdguideme::Application.config.session_store :active_record_store
